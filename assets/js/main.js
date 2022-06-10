5d90365ce710cd5b4876d00ff4b4f7b8e99d259a
anggiwahyuu <anggiwahyu543@gmail.com>
2022-06-10 09:34:34 +0700

    add embed video from youtube and remove modal for temporary

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
-$('.btn-timeline').click(function () {
-    $('.btn-timeline').removeClass('btn-timeline-active');
-    $(this).addClass('btn-timeline-active');
+// $('.btn-timeline').click(function () {
+//     $('.btn-timeline').removeClass('btn-timeline-active');
+//     $(this).addClass('btn-timeline-active');
 
-    var timelineID = $(this).attr('id').replace('btn-', '');
+//     var timelineID = $(this).attr('id').replace('btn-', '');
 
-    $('.timeline-group').removeClass("show-timeline");
-    $('#timeline-' + timelineID).addClass('show-timeline');
-});
+//     $('.timeline-group').removeClass("show-timeline");
+//     $('#timeline-' + timelineID).addClass('show-timeline');
+// });
 
-$(".competition-card").click(function () {
-    var competitionID = $(this).attr('id').replace('toggle-', '');
-    $('#modal-' + competitionID).addClass('modal-show');
-});
+// $(".competition-card").click(function () {
+//     var competitionID = $(this).attr('id').replace('toggle-', '');
+//     $('#modal-' + competitionID).addClass('modal-show');
+// });
 
-$(".modal-competition-container").click(function () {
-    $(this).removeClass('modal-show');
-});
+// $(".modal-competition-container").click(function () {
+//     $(this).removeClass('modal-show');
+// });
 
 $(document).ready(function () {
     $(window).scroll(function () {
